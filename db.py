2c2408fa543b3d37fbed5e2631a35c4b7c7a7091
K Manohar <114045942+Manohar-code3@users.noreply.github.com>
2026-01-07 19:17:08 +0530

    update in db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="db-flask-server.cpgcgoyqc8fb.ap-south-1.rds.amazonaws.com",
-        user="admin",
-        password="admin123",
+        host="with your db end point",
+        user=" your user name",
+        password="password",
         database="usersdb"
     )
